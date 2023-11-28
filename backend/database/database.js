f9ed1eb95ec4c1e13e7b9545474e283a36786427
Mateus Andrade <mateus.andrade@erzinger.local>
2023-11-28 16:46:34 -0300

    tests

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,6 +1,5 @@
-const databaseConfig = () => {
-    if (process.env.ENV === 'dev') {
-      return {
+if(process.env.ENV === 'dev') {
+    module.exports = {
         dialect: 'mysql',
         host: 'localhost',
         timezone: '-03:00',
@@ -8,18 +7,15 @@ const databaseConfig = () => {
         username: 'root',
         password: '',
         database: 'salao'
-      };
-    } else {
-      return {
+    }
+} else {
+    module.exports = {
         dialect: 'mysql',
         host: 'database-portfolio.caa9x9ctigzd.us-east-1.rds.amazonaws.com',
         timezone: '-03:00',
         port: 3306,
         username: process.env.USER,
         password: process.env.PASSWORD,
-        database: 'salao'
-      };
+        database: 'salao' 
     }
-  };
-  
-  module.exports = databaseConfig;
+}
