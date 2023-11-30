99f3446c8267f500a15fb4f08922b5407394cf8c
Mateus Andrade <mateus.andrade@erzinger.local>
2023-11-30 16:46:59 -0300

    tests

--- calendar/src/tests/Agendamento.spec.js
+++ calendar/src/tests/Agendamento.spec.js
@@ -1,5 +1,4 @@
 import { render, screen, waitFor, act } from '@testing-library/react';
-import api from '../services/api'
 import { BrowserRouter as Router } from 'react-router-dom';
 import Agendamento from '../components/Agendamento/Agendamento';
 
--- calendar/src/tests/App.spec.js
+++ calendar/src/tests/App.spec.js
@@ -1,5 +1,4 @@
 import { render, screen, waitFor, act } from '@testing-library/react';
-import api from '../services/api'
 import App from '../App';
 
 describe('Página inicial', () => {
